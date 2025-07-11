7c00190e6f44c8141b172ac1b8e7ac62a46acace
nitinrawat-doc <nitinrawat2906@gmail.com>
2025-07-11 15:43:26 +0000

    comit

--- 01_basics/02_conversionOperation.js
+++ 01_basics/02_conversionOperation.js
@@ -1,11 +1,15 @@
-let score="33abc";
-console.log(typeof score);
-let inNumber=Number(score);
-console.log(typeof inNumber);
-console.log( inNumber);
-let boolId=0;
-let boolConvert=Boolean(boolId);
-console.log(boolConvert);
-let sumNumber=33;
-let stringNumber=String(sumNumber);
-console.log(typeof stringNumber);
+// let score="33abc";
+// console.log(typeof score);
+// let inNumber=Number(score);
+// console.log(typeof inNumber);
+// console.log( inNumber);
+// let boolId=0;
+// let boolConvert=Boolean(boolId);
+// console.log(boolConvert);
+// let sumNumber=33;
+// let stringNumber=String(sumNumber);
+// console.log(typeof stringNumber);
+let str1='nitin';
+let str2=' rawat';
+console.log(str1+str2);
+
