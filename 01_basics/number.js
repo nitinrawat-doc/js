1f4e08364a48ee0de580292ce851e491fecfb33f
nitinrawat-doc <nitinrawat2906@gmail.com>
2025-07-12 09:19:31 +0000

    math and number

---
+++ 01_basics/number.js
@@ -0,0 +1,14 @@
+// let num1=239;
+// console.log(num1.toString());
+// console.log(num1.toString().length);
+// console.log(num1.toFixed(2));
+// let num2=433.33234;
+// console.log(num2.toPrecision(3));
+
+console.log(Math.abs(-5));
+console.log(Math.round(4.65));
+console.log(Math.pow(3, 4));
+console.log(Math.random() * 5);
+console.log(Math.max(3, 5, 6, 7, 3));
+console.log(Math.min(3, 5, 6, 7, 3));
+
